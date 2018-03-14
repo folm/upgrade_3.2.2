6abe4392c8d822a197614dec3e60d406a440291d
Folm Coin <35082363+folm@users.noreply.github.com>
2018-03-14 16:23:39 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 clear
 
+# backup
+mkdir ~/folm_backup
+cp -r ~/.folm/wallet.dat ~/folm_backup
+cp -r ~/.folm/folm.conf ~/folm_backup
+cp -r ~/.folm/masternode.conf ~/folm_backup
+
+#get last version
 wget https://github.com/folm/folm/releases/download/v3.2.2/folm-3.2.2-ubuntu16.04.zip
 unzip folm-3.2.2-ubuntu16.04.zip
 cd folm-3.2.2-ubuntu16.04
